c5bdc339512a3b27b74e7833b90e66dcb33f76dc
Andres-XL <amovilla1103@gmail.com>
2024-02-02 16:06:13 -0500

    this sh*t uses formdata

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,16 +7,17 @@ const API = axios.create({
 });
 
 export function findUser(code: string) {
-  return API.post(`oauth/access_token/`, {
-    params: {
-      client_id: process.env.instagramClientId,
-      client_secret: process.env.instagramClientSecret,
-      grant_type: "authorization_code",
-      redirect_uri: "https://follow-count-announce.vercel.app/",
-      code: code,
-    },
+  const formData = new FormData();
+  formData.append("client_id", process.env.instagramClientId!);
+  formData.append("client_secret", process.env.instagramClientSecret!);
+  formData.append("grant_type", "authorization_code");
+  formData.append("redirect_uri", "https://follow-count-announce.vercel.app/");
+  formData.append("code", code);
+
+  return API.post(`oauth/access_token/`, formData).then(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return
-  }).then((r): any => r.data);
+    (r): any => r.data,
+  );
 }
 
 export default API;
