757975c80ca62e09f8f5de9103a3b97491f92899
Andres-XL <amovilla1103@gmail.com>
2024-02-02 22:54:28 -0500

    split code loading to separate page

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Suspense } from "react";
+import LoadSearchParamCode from "../_components/load-search-param-code";
+import { useFollowStore } from "~/store/followStore";
+
+export default function Home() {
+  const { authCode, authCodeResolved } = useFollowStore();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <Suspense>
+        <LoadSearchParamCode />
+      </Suspense>
+    </main>
+  );
+}
+44;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import InstagramLogin from "./_components/instagram-login";
 import FollowerCount from "./_components/follower-count";
-import { Suspense, useEffect } from "react";
-import { findUser } from "~/api";
-import LoadSearchParamCode from "./_components/load-search-param-code";
 import { useFollowStore } from "~/store/followStore";
 
 export default function Home() {
@@ -13,9 +9,6 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <Suspense>
-        <LoadSearchParamCode />
-      </Suspense>
       {!authCode && authCodeResolved && <InstagramLogin />}
       {authCode && authCodeResolved && <FollowerCount />}
     </main>
