fee30d388e0c896929eec48170aeed4109a2fdb5
Andres-XL <amovilla1103@gmail.com>
2024-02-06 19:48:43 -0500

    back to form data

--- src/app/get-oauth/route.ts
+++ src/app/get-oauth/route.ts
@@ -9,27 +9,21 @@ const API = axios.create({
 });
 
 export async function POST(request: Request) {
-  // const { user_id, client_secret, grant_type, redirect_uri, code } = request.body// as Record<string, string>;
-
-  // const formData = new FormData();
-  // formData.append("user_id", user_id);
-  // formData.append("client_secret", client_secret);
-  // formData.append("grant_type", grant_type);
-  // formData.append("redirect_uri", redirect_uri);
-  // formData.append("code", code);
-  console.log({ body: request.body });
+  const formData = await request.formData();
+  console.log({ formData });
+  console.log(formData);
 
   const data = await API.post<{ access_token: string; user_id: number }>(
     `oauth/access_token/`,
-    request.body,
+    formData,
   )
     .then((r) => {
       console.log({ data: r.data });
 
       return r.data;
     })
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     .catch((e) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       console.log({ e });
     });
 
