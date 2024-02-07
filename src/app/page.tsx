15b7f3d9fe8a1ddb439c05b3731deae6e2277816
Andres-XL <amovilla1103@gmail.com>
2024-02-06 21:35:05 -0500

    fuck instagram, make the timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,75 @@
 "use client";
 
-import InstagramLogin from "../components/instagram-login";
-import FollowerCount from "../components/follower-count";
+import { Temporal } from "@js-temporal/polyfill";
+import { useEffect, useState } from "react";
+// import InstagramLogin from "../components/instagram-login";
+// import FollowerCount from "../components/follower-count";
 import { useFollowStore } from "~/store/followStore";
 
 export default function Home() {
-  const {
-    shortLivedToken: authCode,
-    shortLivedTokenResolved: authCodeResolved,
-  } = useFollowStore();
+  const [dueTime, setDueTime] = useState(
+    Temporal.Now.instant().add({
+      //  minutes: 1
+      seconds: 10 + 2,
+    }),
+  );
+  const [remaining, setRemaining] = useState({ minutes: "15", seconds: "00" });
+  const [mode, setMode] = useState<"open" | "closed">("closed");
+
+  useEffect(() => {
+    console.log("created the interval");
+
+    const interval = setInterval(() => {
+      const { minutes, seconds } = Temporal.Now.instant().until(dueTime, {
+        largestUnit: "minutes",
+        smallestUnit: "seconds",
+      });
+
+      setRemaining({
+        minutes:
+          minutes < 0 ? "00" : minutes < 10 ? `0${minutes}` : `${minutes}`,
+        seconds:
+          seconds < 0 ? "00" : seconds < 10 ? `0${seconds}` : `${seconds}`,
+      });
+      console.log(seconds);
+
+      if (seconds < -5) {
+        const difference = mode === "closed" ? 5 : 10;
+        setDueTime(Temporal.Now.instant().add({ seconds: difference + 2 }));
+        setMode(mode === "closed" ? "open" : "closed");
+      }
+    }, 1000);
+    return () => {
+      console.log("cleared the interval");
+
+      clearInterval(interval);
+    };
+  }, [dueTime]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <InstagramLogin />
-      {/* {authCode && authCodeResolved && <FollowerCount />} */}
+    <main
+      className={`flex min-h-screen flex-col items-center justify-center bg-gradient-to-b ${mode === "open" ? "from-[#026d19]" : "from-[#6d0202]"} to-[#15162c] text-white`}
+    >
+      {/* <InstagramLogin / */}
+      <div className="flex gap-3 text-[30rem] font-bold">
+        {remaining.minutes.split("").map((num, index) => (
+          <div
+            className="min-w-[18vw] max-w-[18vw] rounded-xl bg-white text-center text-black"
+            key={index}
+          >
+            {num}
+          </div>
+        ))}
+        :
+        {remaining.seconds.split("").map((num, index) => (
+          <div
+            className="min-w-[18vw] max-w-[18vw] rounded-xl bg-white text-center text-black"
+            key={index}
+          >
+            {num}
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
